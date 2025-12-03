98036d132a097a8388c368abc3b5512031fda975
Dhairya Shukla <113496448+dhairyashukla08@users.noreply.github.com>
2025-12-03 13:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function insert_Row() {
  let table=document.getElementById("sampleTable");
 	let row=table.insertRow(0);
 	let cell1=row.insertCell(0);
-	cell1.innerHTMl= "New Cell1";
+	cell1.innerHTML= "New Cell1";
 
-	let cell2=row.inserCell(1);
+	let cell2=row.insertCell(1);
 	cell2.innerHTML= "New Cell2";
   
   
